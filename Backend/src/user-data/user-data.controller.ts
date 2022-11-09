2f759219d60950cfed2e9a2037319bdedaaf8da2
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2022-11-09 19:39:38 +0530

    Update Changes

--- Backend/src/user-data/user-data.controller.ts
+++ Backend/src/user-data/user-data.controller.ts
@@ -63,4 +63,148 @@ export class UserDataController {
         const response = await this.userDataService.findYesterday6Data();
         return response;
     }
+
+    @Get('findYesterday7Data')
+    async findYesterday7Data() {
+        const response = await this.userDataService.findYesterday7Data();
+        return response;
+    }
+
+    @Get('findYesterday8Data')
+    async findYesterday8Data() {
+        const response = await this.userDataService.findYesterday8Data();
+        return response;
+    }
+
+    @Get('findYesterday9Data')
+    async findYesterday9Data() {
+        const response = await this.userDataService.findYesterday9Data();
+        return response;
+    }
+
+    @Get('findYesterday10Data')
+    async findYesterday10Data() {
+        const response = await this.userDataService.findYesterday10Data();
+        return response;
+    }
+
+    @Get('findYesterday11Data')
+    async findYesterday11Data() {
+        const response = await this.userDataService.findYesterday11Data();
+        return response;
+    }
+
+    @Get('findYesterday12Data')
+    async findYesterday12Data() {
+        const response = await this.userDataService.findYesterday12Data();
+        return response;
+    }
+
+    @Get('findYesterday13Data')
+    async findYesterday13Data() {
+        const response = await this.userDataService.findYesterday13Data();
+        return response;
+    }
+
+    @Get('findYesterday14Data')
+    async findYesterday14Data() {
+        const response = await this.userDataService.findYesterday14Data();
+        return response;
+    }
+
+    @Get('findYesterday15Data')
+    async findYesterday15Data() {
+        const response = await this.userDataService.findYesterday15Data();
+        return response;
+    }
+
+    @Get('findYesterday16Data')
+    async findYesterday16Data() {
+        const response = await this.userDataService.findYesterday16Data();
+        return response;
+    }
+
+    @Get('findYesterday17Data')
+    async findYesterday17Data() {
+        const response = await this.userDataService.findYesterday17Data();
+        return response;
+    }
+
+    @Get('findYesterday18Data')
+    async findYesterday18Data() {
+        const response = await this.userDataService.findYesterday18Data();
+        return response;
+    }
+
+    @Get('findYesterday19Data')
+    async findYesterday19Data() {
+        const response = await this.userDataService.findYesterday19Data();
+        return response;
+    }
+
+    @Get('findYesterday20Data')
+    async findYesterday20Data() {
+        const response = await this.userDataService.findYesterday20Data();
+        return response;
+    }
+
+    @Get('findYesterday21Data')
+    async findYesterday21Data() {
+        const response = await this.userDataService.findYesterday21Data();
+        return response;
+    }
+
+    @Get('findYesterday22Data')
+    async findYesterday22Data() {
+        const response = await this.userDataService.findYesterday22Data();
+        return response;
+    }
+
+    @Get('findYesterday23Data')
+    async findYesterday23Data() {
+        const response = await this.userDataService.findYesterday23Data();
+        return response;
+    }
+
+    @Get('findYesterday24Data')
+    async findYesterday24Data() {
+        const response = await this.userDataService.findYesterday24Data();
+        return response;
+    }
+
+    @Get('findYesterday25Data')
+    async findYesterday25Data() {
+        const response = await this.userDataService.findYesterday25Data();
+        return response;
+    }
+
+    @Get('findYesterday26Data')
+    async findYesterday26Data() {
+        const response = await this.userDataService.findYesterday26Data();
+        return response;
+    }
+
+    @Get('findYesterday27Data')
+    async findYesterday27Data() {
+        const response = await this.userDataService.findYesterday27Data();
+        return response;
+    }
+
+    @Get('findYesterday28Data')
+    async findYesterday28Data() {
+        const response = await this.userDataService.findYesterday28Data();
+        return response;
+    }
+
+    @Get('findYesterday29Data')
+    async findYesterday29Data() {
+        const response = await this.userDataService.findYesterday29Data();
+        return response;
+    }
+
+    @Get('getvalueofspecificdate/:date')
+    async getvalueofspecificdate(@Param('date') date: Date) {
+        const response = await this.userDataService.getvalueofspecificdate(date);
+        return response;
+    }
 }
--- Backend/src/user-data/user-data.service.ts
+++ Backend/src/user-data/user-data.service.ts
@@ -47,19 +47,162 @@ export class UserDataService {
 
   findYesterday4Data(): Promise<UserDatum[]> {
     let today = moment().format("YYYY-MM-DD");
-    let date = moment(today).subtract(1, 'days').format("YYYY-MM-DD");
+    let date = moment(today).subtract(4, 'days').format("YYYY-MM-DD");
     return this.userDataRepository.find({ where: { AddedDate: date } });
   }
 
   findYesterday5Data(): Promise<UserDatum[]> {
     let today = moment().format("YYYY-MM-DD");
-    let date = moment(today).subtract(2, 'days').format("YYYY-MM-DD");
+    let date = moment(today).subtract(5, 'days').format("YYYY-MM-DD");
     return this.userDataRepository.find({ where: { AddedDate: date } });
   }
 
   findYesterday6Data(): Promise<UserDatum[]> {
     let today = moment().format("YYYY-MM-DD");
-    let date = moment(today).subtract(3, 'days').format("YYYY-MM-DD");
+    let date = moment(today).subtract(6, 'days').format("YYYY-MM-DD");
     return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday7Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(7, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday8Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(8, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday9Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(9, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday10Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(10, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday11Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(11, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday12Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(12, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday13Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(13, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday14Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(14, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday15Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(15, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday16Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(16, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday17Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(17, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday18Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(18, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday19Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(19, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday20Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(20, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday21Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(21, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday22Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(22, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday23Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(23, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday24Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(24, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday25Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(25, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday26Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(26, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday27Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(27, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday28Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(28, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  findYesterday29Data(): Promise<UserDatum[]> {
+    let today = moment().format("YYYY-MM-DD");
+    let date = moment(today).subtract(29, 'days').format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: date } });
+  }
+
+  getvalueofspecificdate(date: Date): Promise<UserDatum[]> {
+    let gettingDate = moment(date).format("YYYY-MM-DD");
+    return this.userDataRepository.find({ where: { AddedDate: gettingDate } });
   }
 }
